79c9068958d176820ac1b4f74751c8d7091dd9c6
Shadofer <shadowrlrs@gmail.com>
2022-12-21 20:36:48 +0200

    Add `XMAS` background

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
   import Button from "@smui/button";
+  import Background from "./lib/Background.svelte";
   import Banner from "./lib/Banner.svelte";
   import Form from "./lib/Form.svelte";
   import Result from "./lib/Result.svelte";
   import { isGenerated } from "./stores";
 </script>
+
+<Background />
 
 <Banner />
 <main>
@@ -33,13 +36,6 @@
     align-items: center;
     min-height: inherit;
     flex-direction: column;
-  }
-  :global(body) {
-    background: rgb(23, 18, 116);
-    background: linear-gradient(
-      180deg,
-      rgba(23, 18, 116, 1) 0%,
-      rgba(26, 68, 156, 1) 100%
-    );
+    margin: auto;
   }
 </style>
