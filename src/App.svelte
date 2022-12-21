81dfd9e4989c1ef5c47553c5493c91077f027bfc
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-12-21 10:15:22 +0100

    Add gradient

--- src/App.svelte
+++ src/App.svelte
@@ -13,4 +13,14 @@
     min-height: inherit;
     flex-direction: column;
   }
+  :global(body) {
+    background: linear-gradient(
+      to bottom,
+      #3b4371,
+      #575b89,
+      #6a6f9e,
+      #7d84b4,
+      #9198ca
+    );
+  }
 </style>
