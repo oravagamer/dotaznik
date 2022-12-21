c96eb066d4af6cd63c87023595b4d6aaf315d4bc
Shadofer <shadowrlrs@gmail.com>
2022-12-21 21:35:44 +0200

    Add `prettier`

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -1,14 +1,13 @@
-<div></div>
-
+<div />
 
 <style>
-    div {
-        position: absolute;
-        top: 0;
-        right: 0;
-        left: 0;
-        bottom: 0;
-        filter: blur(5px);
-        background: url(https://wallpapercave.com/wp/1zIVRdg.jpg);
-    }
+  div {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    filter: blur(5px);
+    background: url(https://wallpapercave.com/wp/1zIVRdg.jpg);
+  }
 </style>
