90aa16e7b342681c66ba9eb4c99e7e19a9fab50b
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-12-21 09:56:07 +0100

    Add radio options

--- src/lib/Select.svelte
+++ src/lib/Select.svelte
@@ -1,87 +1,59 @@
 <script>
-  // Import the selectedValues store and destructure the selected1, selected2, and selected3 values
-  import { selectedValues } from "../stores.js";
-  let { selected1, selected2, selected3 } = $selectedValues;
+  import { selectedOption } from "../stores";
 
-  // Create an array of options for each select box
-  let options1 = ["Option 1", "Option 2", "Option 3"];
-  let options2 = ["Option A", "Option B", "Option C"];
-  let options3 = ["Option X", "Option Y", "Option Z"];
+  function handleChange(event) {
+    $selectedOption.set(event.target.value);
+  }
 </script>
 
-<!-- Render the select boxes with their respective options and bind the selected value to the corresponding store value -->
-<div>
-  <select bind:value={selected1}>
-    {#each options1 as option}
-      <option value={option}>{option}</option>
-    {/each}
-  </select>
-
-  <select bind:value={selected2}>
-    {#each options2 as option}
-      <option value={option}>{option}</option>
-    {/each}
-  </select>
-
-  <select bind:value={selected3}>
-    {#each options3 as option}
-      <option value={option}>{option}</option>
-    {/each}
-  </select>
-</div>
-<!-- Display the selected values -->
-<p>Selected values: {selected1}, {selected2}, {selected3}</p>
-
-<style>
-  /* styles.css */
-
-  /* Apply material design styles to the select element */
-  select {
-    display: block;
-    height: 40px;
-    font-size: 16px;
-    margin: 10px 0;
-    padding: 10px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    outline: none;
-    background-color: white;
-    color: rgba(0, 0, 0, 0.87);
-  }
-
-  /* Apply material design styles to the options within the select element */
-  select option {
-    font-weight: normal;
-  }
-
-  /* Apply material design styles to the label for the select element */
-  label[for^="select"] {
-    display: block;
-    font-size: 16px;
-    margin-bottom: 5px;
-  }
-
-  /* Apply material design styles to the placeholder text in the select element */
-  select::-ms-expand {
-    display: none;
-  }
-
-  /* Use the ::placeholder pseudo-element to style the placeholder text */
-  select::placeholder {
-    color: rgba(0, 0, 0, 0.38);
-  }
-
-  /* Use the :focus pseudo-class to style the select element when it has focus */
-  select:focus {
-    border-color: #2196f3;
-  }
-
-  /* Use the :disabled pseudo-class to style the select element when it is disabled */
-  select:disabled {
-    color: rgba(0, 0, 0, 0.38);
-    border-color: rgba(0, 0, 0, 0.12);
-    background-color: rgba(0, 0, 0, 0.12);
-    cursor: not-allowed;
-  }
-</style>
+<form>
+  <label>
+    <input
+      type="radio"
+      name="option"
+      value="option1"
+      on:change={handleChange}
+    />
+    Option 1
+  </label>
+  <br />
+  <label>
+    <input
+      type="radio"
+      name="option"
+      value="option2"
+      on:change={handleChange}
+    />
+    Option 2
+  </label>
+  <br />
+  <label>
+    <input
+      type="radio"
+      name="option"
+      value="option3"
+      on:change={handleChange}
+    />
+    Option 3
+  </label>
+  <br />
+  <label>
+    <input
+      type="radio"
+      name="option"
+      value="option4"
+      on:change={handleChange}
+    />
+    Option 4
+  </label>
+  <br />
+  <label>
+    <input
+      type="radio"
+      name="option"
+      value="option5"
+      on:change={handleChange}
+    />
+    Option 5
+  </label>
+</form>
--- src/stores.js
+++ src/stores.js
@@ -1,8 +1,3 @@
 import { writable } from "svelte/store";
 
-// Create the store and set the initial values
-export const selectedValues = writable({
-  selected1: "Option 1",
-  selected2: "Option A",
-  selected3: "Option X",
-});
+export const selectedOption = writable(null);
